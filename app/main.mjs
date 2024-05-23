2211459de3ed4ce27fbfcacda87f28c95b1b7edc
Cabie Wu <peterjuju99@gmail.com>
2024-05-23 13:23:36 +0800

    [Refactor] 導入airbnb eslint規則，並且依規則格式化文件

--- app/main.mjs
+++ app/main.mjs
@@ -1,17 +1,17 @@
 import isDev from 'electron-is-dev';
 import { app, BrowserWindow } from 'electron';
 
-function createWindow () {
+function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 1024,
-        height: 900
-    })
+        height: 900,
+    });
 
     if (isDev) {
         // 開發階段直接與 React 連線
         mainWindow.loadURL('http://localhost:3000/');
         // 開啟 DevTools.
-        mainWindow.webContents.openDevTools()
+        mainWindow.webContents.openDevTools();
     } else {
         // 產品階段直接讀取 React 打包好的
         mainWindow.loadFile('./build/index.html');
@@ -19,12 +19,12 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
